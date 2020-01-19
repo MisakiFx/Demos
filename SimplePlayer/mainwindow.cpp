7fce878c4aabe765d02b8a569eee4ddcaf3cf414
Misaki <1761607418@qq.com>
2020-01-20 02:26:34 +0800

    2020.1.20

--- SimplePlayer/mainwindow.cpp
+++ SimplePlayer/mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -21,14 +23,106 @@ MainWindow::MainWindow(QWidget *parent) :
     _mediaPlayer = new QMediaPlayer(this);
     //设置对应mediaPlayer 的QVideoWidget
     _mediaPlayer->setVideoOutput(ui->videoWidget);
+    //设置播放属性
+    //1、监听信号变化
+    connect(_mediaPlayer, SIGNAL(stateChanged(QMediaPlayer::State)), this, SLOT(MediaStateChanged(QMediaPlayer::State)));
+    //2、播放进度变化函数
+    connect(_mediaPlayer, SIGNAL(positionChanged(qint64)), this, SLOT(PositionChanged(qint64)));
+    //3、媒体播放长度变化
+    connect(_mediaPlayer, SIGNAL(durationChanged(qint64)), this, SLOT(DurationChanged(qint64)));
+    //4、设置播放错误关联
+    connect(_mediaPlayer, SIGNAL(error(QMediaPlayer::Error)), this, SLOT(HandleError()));
+    //设置播放内容
+    //_mediaPlayer->setMedia(QUrl::fromLocalFile("D:/KK/KK_Movies/1.mp4"));
 
     //设置播放按钮
     ui->pSButton->setToolTip("播放");//设置点击提示内容
     ui->pSButton->setAutoRaise(true);//设置自动弹起
+    ui->pSButton->setEnabled(false);
     ui->pSButton->setIcon(QPixmap(":/image/play.png"));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::Play()
+{
+    //获取播放状态
+    switch(_mediaPlayer->state())
+    {
+        case QMediaPlayer::PlayingState:
+            _mediaPlayer->pause();
+            break;
+        default:
+            _mediaPlayer->play();
+            break;
+    }
+}
+
+void MainWindow::on_pSButton_clicked()
+{
+    Play();
+
+    //设置播放按钮暂停样式
+//    ui->pSButton->setToolTip("暂停");
+//    ui->pSButton->setIcon(QPixmap(":/image/stop.png"));
+}
+
+void MainWindow::MediaStateChanged(QMediaPlayer::State state)
+{
+    switch(state)
+    {
+        case QMediaPlayer::PlayingState:
+            ui->pSButton->setToolTip("暂停");
+            ui->pSButton->setIcon(QPixmap(":/image/stop.png"));
+            break;
+        default:
+            ui->pSButton->setToolTip("播放");
+            ui->pSButton->setIcon(QPixmap(":/image/play.png"));
+            break;
+    }
+}
+
+void MainWindow::PositionChanged(qint64 position)
+{
+    ui->playSlider->setValue(position);
+}
+
+void MainWindow::SetPositon(int position)
+{
+    _mediaPlayer->setPosition(position);
+}
+
+void MainWindow::DurationChanged(qint64 duration)
+{
+    ui->playSlider->setRange(0, duration);
+}
+
+void MainWindow::on_playSlider_sliderMoved(int position)
+{
+    if(_mediaPlayer->state() == QMediaPlayer::StoppedState)
+    {
+        ui->playSlider->setValue(0);
+    }
+    else
+    {
+        SetPositon(position);
+    }
+}
+
+void MainWindow::HandleError()
+{
+    ui->pSButton->setEnabled(false);
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString moviePath = QFileDialog::getOpenFileName(this, tr("Open Media"),
+                                                     "/home",
+                                                     tr("Media (*.mp4 *.wav *.mp3)"));
+    _mediaPlayer->setMedia(QUrl::fromLocalFile(moviePath));
+    ui->pSButton->setEnabled(true);
 }
--- SimplePlayer/mainwindow.h
+++ SimplePlayer/mainwindow.h
@@ -23,13 +23,24 @@ private:
 
 private slots:
     //播放
-    void play();
-    //获取播放状态
-    void mediaStateChanged(QMediaPlayer::State state);
+    void Play();
+    //播放过程中动态调整播放进度
+    void MediaStateChanged(QMediaPlayer::State state);
     //改变播放位置
-    void positionChanged(qint64 position);
-    //获取播放位置
-    void setPositon(int position);
+    void PositionChanged(qint64 position);
+    //设置进度条调整位置
+    void SetPositon(int position);
+    //设置进度条范围
+    void DurationChanged(qint64 duration);
+    //播放异常的抛出
+    void HandleError();
+
+    //开始暂停按钮
+    void on_pSButton_clicked();
+    //进度条
+    void on_playSlider_sliderMoved(int position);
+    //菜单栏
+    void on_actionOpen_triggered();
 };
 
 #endif // MAINWINDOW_H
